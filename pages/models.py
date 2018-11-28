cf61d1220216f6f9e3e80a95fdc3fa3ae61c5624
“AlaaAlsubaie” <eng_a.a.a@hotmail.com>
2018-11-28 20:38:59 +0300

    alaa-farah-wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length = 120)
+	content = models.TextField()
+	last_update = models.DateTimeField()
+
+
+	def __str__(self):
+	   return self.title
+
+	
+	def get_absolute_url(self):
+		return reverse('page-detail', args=[str(self.id)])
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+
+def list_view(request):
+	context = {
+		"page_list": Page.objects.all(),
+	}
+
+	return render(request, "list_page.html" , context)
+
+
+def detail_view(request,page_id):
+	
+	context = {
+	 "page_detail": Page.objects.get(id=page_id)
+	}
+	return render(request, "detail_page.html" , context)
